51ad30eaca1b1b93089b7433086a165c4324e8fc
Christian Schmitz <christian.schmitz@be.atlascopco.com>
2016-11-16 11:49:14 +0100

    optimization working, TODO: cleanup and profiling

--- krr.py
+++ krr.py
@@ -13,8 +13,8 @@ def getParser():
     parser.add_argument("-n", "--num-indep", type=int, default=0, help="number of independent variables")
     parser.add_argument("-r", "--ranges", help="comma separated list of ranges, eg: a0:a1:Na:max(2),b0:b1,c,d,e. Overrides indep variables from FILE or RANGE-FILE")
     parser.add_argument("-f", "--range-file", help="overrides the indep variables in FILE")
-    parser.add_argument("-s", "--sigma", help="gaussian kernel deviation, default=1.0, can be comma separated list for multiple observed variables", default="1.0")
-    parser.add_argument("-a", "--alpha", help="penalization parameter, default=0.1, can be comma separated list for multiple observed variables", default="0.1")
+    parser.add_argument("-s", "--sigma", help="gaussian kernel deviation, default=0.5, can be comma separated list for multiple observed variables", default="0.5")
+    parser.add_argument("-a", "--alpha", help="penalization parameter, default=0.0002, can be comma separated list for multiple observed variables", default="0.0002")
     return parser
 
 
@@ -31,10 +31,13 @@ def parseArgs():
             for rangeI in range(0,len(ranges)):
                 rangeTokens = ranges[rangeI].split(":")
                 if (len(rangeTokens) == 4):
-                    ranges[rangeI] = [float(rangeTokens[0]), float(rangeTokens[1]),
-                            float(rangeTokens[2]), rangeTokens[3]]
+                    ranges[rangeI] = [float(rangeTokens[0]), 
+                            float(rangeTokens[1]),
+                            float(rangeTokens[2]), 
+                            rangeTokens[3]]
                 else:
-                    ranges[rangeI]=[float(i) for i in ranges[rangeI].split(":")[:-1]]
+                    ranges[rangeI]=\
+                    [float(i) for i in ranges[rangeI].split(":")]
                 if len(ranges[rangeI]) > 4:
                     raise Exception("")
         except:
@@ -53,12 +56,13 @@ def parseArgs():
 
 
 def flattenMeshGrid(linSpaces):
-    meshGrid = np.meshgrid(tuple(linSpaces))
+    meshGrid = np.meshgrid(*linSpaces)
     # Now concat these horizontally
     numPoints = meshGrid[0].size
     x = np.zeros((numPoints, len(meshGrid)))
     for i in range(0,len(meshGrid)):
         x[:,i] = np.reshape(meshGrid[i], (numPoints))
+    #print(x)
     return x
 
 
@@ -76,12 +80,12 @@ def getPredVarsFromRanges(ranges):
             indexSpaces.append(np.array([int(i) for i in np.linspace(0,defaultNum-1,defaultNum, endpoint=True)[:]]))
         elif (len(r) == 1):
             linSpaces.append(np.array([r[0]]))
-            indexSpace.append(np.array([0]))
+            indexSpaces.append(np.array([0]))
         else:
             raise Exception("Error in ranges: "+str(r))
 
     predVars = flattenMeshGrid(linSpaces)
-    ndIndexing = flattenMeshGrid(indexSpaces)
+    ndIndexing = flattenMeshGrid(indexSpaces).astype(int)
 
     return (predVars, linSpaces, ndIndexing)
 
@@ -184,12 +188,33 @@ def predictObserved(fname, predVars, sigma, alpha):
 
 
 def parseOptimizationToken(string):
-    tokens = string.split('(')
-    tokens = [tokens[0]]+tokens[1].split(')')
+    tokens = []
+    tokens.append(string[0:3])
+    tokens.append(string[3:])
 
     mode = tokens[0]
-    colI = int(tokens[1])
+    colI = int(tokens[1])-1
     return (mode, colI)
+
+
+def applyReductionMask(field, indices):
+    data = np.empty(indices.shape)
+    for idx_tup, zidx in np.ndenumerate(indices):
+        data[idx_tup] = field[idx_tup+(zidx,)]
+    return data
+
+
+def applyReductionMask2(field, indices, axis):
+    magic_index = [np.arange(i) for i in indices.shape]
+    magic_index = np.ix_(*magic_index)
+    magic_index = magic_index[:axis] + (indices,) + magic_index[axis:]
+    return field[magic_index]
+
+
+def applyReductionMask3(field, indices, axis):
+    magic_index = np.ogrid[tuple(slice(i) for i in indices.shape)]
+    magic_index.insert(axis, indices)
+    return field[magic_index]
 
 
 def optimizePredVars(allData, numIndep, ranges, linSpaces, ndIndexing):
@@ -208,7 +233,7 @@ def optimizePredVars(allData, numIndep, ranges, linSpaces, ndIndexing):
 
     sizes = []
     for i in range(0,numIndep):
-        sizes.append(int(np.amax(ndIndexing[:,i])))
+        sizes.append(int(np.amax(ndIndexing[:,i]))+1)
 
     data = []
     for j in range(0,numAll):
@@ -217,7 +242,8 @@ def optimizePredVars(allData, numIndep, ranges, linSpaces, ndIndexing):
 
         # loop the data, putting the predOut vars in there
         for i in range(0, numPoints):
-            matrix[ndIndexing[i,:]] = allData[i,j]
+            idx = tuple(ndIndexing[i,:].tolist())
+            matrix[idx] = allData[i,j]
         data.append(matrix)
 
     ## maximize along certain axes, do this by looping the ranges
@@ -225,26 +251,43 @@ def optimizePredVars(allData, numIndep, ranges, linSpaces, ndIndexing):
         r = ranges[i]
         if (len(r) == 4):
             if (r[3] > numIndep-1):
-                (mode, colI) = parseOptmizationToken(r[3])
-                optimI = colI - numIndep
+                (mode, colI) = parseOptimizationToken(r[3])
+                #optimI = colI - numIndep
+                allIDS = np.indices(data[colI].shape)
+                #print(data[colI])
                 if (mode == 'max'):
-                    optimIDS = np.argmax(data[optimI], axis=i)
-                    data[optimI] = np.amax(data[optimI], axis=i)
+                    #optimIDS = np.unravel_index(np.argmax(data[colI], axis=i), data[colI].shape)
+                    optimIDS = data[colI].argmax(axis=i)
+                    data[colI] = np.amax(data[colI], axis=i)
                 elif (mode == 'min'):
-                    optimIDS = np.argmin(data[optimI], axis=i)
-                    data[optimI] = np.amin(data[optimI], axis=i)
+                    optimIDS = data[colI].argmin(axis=i)
+                    data[colI] = np.amin(data[colI], axis=i)
                 else:
                     raise Exception("Mode \""+str(mode)+"\" not recognized")
+                #print(data[colI])
+
                 # Loop the other data in order to do the same reduction
+                invOptimIDS=np.delete(ndIndexing, optimIDS, axis=i)
+                invOptimIDS=np.delete(allIDS, optimIDS, axis=i)
                 for j in range(0,numAll):
-                    if (not j == int(r[3])):
-                        data[j] = data[j][optimIDS]
+                    #print("Orig: ")
+                    #print(data[j])
+                    if (not j == colI):
+                        idx = optimIDS
+                        #print(idx)
+                        #print(invOptimIDS)
+                        #data[j] = data[j][optimIDS]
+                        data[j] = applyReductionMask2(data[j], optimIDS, i)
+                    #print("Reduced: ")
+                    #print(data[j])
 
             else:
                 raise Exception("Invalid column indexing for optimizing utility")
     
     ## Convert the matrix back to the flattened form
     allData = data[0].reshape((data[0].size))
+    #print(allData)
+    #print(allData.size)
     for i in range(1,numAll):
         allData = np.c_[allData, data[i].reshape((data[1].size))]
     return allData
@@ -258,10 +301,12 @@ def main():
 
     predOut = predictObserved(fname, predVars, sigma, alpha)
 
-    (predVars, predOut) = optimizePredVars(predVars, predOut, ranges, linSpaces, ndIndexing)
-
     dataOut = np.c_[predVars, predOut]
-    dataOut = optimizePredVars(dataOut, numIndep, ranges, linSpaces, ndIndexing)
+    #print(dataOut)
+    if ranges:
+        #print("Read ranges :" + str(ranges))
+        #print("numIndep : " +str(numIndep))
+        dataOut = optimizePredVars(dataOut, numIndep, ranges, linSpaces, ndIndexing)
 
     np.savetxt(sys.stdout, dataOut, fmt='%g')
     return
